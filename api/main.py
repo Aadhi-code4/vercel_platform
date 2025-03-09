ee434f36c2db2bdfc4d369e36606a34c957dccea
Aadhi-code4 <aadhilingam57910@gmail.com>
2025-03-09 10:36:09 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import os
@@ -14,14 +14,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load data from data/q-ver.json
+# Load data from data/students.json
 data_file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'students.json')
 with open(data_file_path) as f:
     students = json.load(f)
 
 @app.get("/api")
-def get_marks(name: str):
-    names = name.split(',')
+def get_marks(names: list[str] = Query(None)):
     marks = [student['marks'] for student in students if student['name'] in names]
     return {"marks": marks}
 
